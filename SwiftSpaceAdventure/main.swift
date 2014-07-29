d152f2018d7141ba7e04684ba1ace94959b49087
Andy Leavitt <andyleavitt@gmail.com>
2014-07-28 21:27:49 -0600

    Initial Commit

---
+++ SwiftSpaceAdventure/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  SwiftSpaceAdventure
+//
+//  Created by Andrew Leavitt on 7/28/14.
+//  Copyright (c) 2014 Andy. All rights reserved.
+//
+
+import Cocoa
+
+NSApplicationMain(C_ARGC, C_ARGV)
